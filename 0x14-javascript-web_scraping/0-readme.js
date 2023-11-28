a680ae3733ea7ece9c78491a20b69469d0195712
Francaicey <francawhyte447@gmail.com>
2023-11-28 03:28:30 -0800

    Read me

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,23 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
-});
+
+function readAndPrintFile(filePath) {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            console.error(`An error occurred: ${err}`);
+        } else {
+            console.log(data);
+        }
+    });
+}
+
+// Check if the correct number of command-line arguments is provided
+if (process.argv.length !== 3) {
+    console.log('Usage: node script_name.js <file_path>');
+} else {
+    // Get the file path from the command-line arguments
+    const filePath = process.argv[2];
+
+    // Call the function to read and print the file content
+    readAndPrintFile(filePath);
+}
